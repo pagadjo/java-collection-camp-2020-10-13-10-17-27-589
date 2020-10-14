7575f56efba9a63fdf3ead6fe8951d1a43486243
JOHN CARLOS PAGADUAN (GEDI-ISD-OOCLL/MNL) <PAGADJO@OOCL.COM>
2020-10-14 17:49:03 +0800

    [Collections][John Carlos]:Feature - modified Reduce.java to satisfy ReduceTest.java's expected results.

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -2,7 +2,11 @@ package com.thoughtworks.collection;
 
 import org.apache.commons.lang3.NotImplementedException;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Reduce {
 
@@ -13,14 +17,19 @@ public class Reduce {
     }
 
     public double getAverage() {
-        throw new NotImplementedException();
+        Double sum = Double.valueOf(arrayList.stream()
+                .reduce(0, (number1, number2) -> (number1 + number2)))
+                / arrayList.size();
+        return sum;
+//
+//        return arrayList.stream().map(Integer::doubleValue).reduce(Double::sum).map(aDouble -> aDouble / arrayList.size());
     }
 
     public int getMaxValue() {
-        throw new NotImplementedException();
+        return arrayList.stream().reduce(Integer.MIN_VALUE, Integer::max);
     }
 
     public int getLastOdd() {
-        throw new NotImplementedException();
+        return arrayList.stream().reduce(0, (max, current) -> current % 2 == 1 ? current : max);
     }
 }
